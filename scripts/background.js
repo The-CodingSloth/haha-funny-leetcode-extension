ab988cdd8b609955298fc1a51bcd0fc73e7cabf3
Korey Moore <kmooredev@gmail.com>
2023-09-07 06:14:53 -0500

    changed the updateStorage alarm to fire at midnight rather than 24 hours after extension install .. I think 🤞

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,15 @@
 //Constants
-const LEETCODE_URL = 'https://leetcode.com';
+const LEETCODE_URL = "https://leetcode.com";
 const RULE_ID = 1;
 // Helper functions
 const isLeetCodeUrl = (url) => url.includes(LEETCODE_URL);
 
 const isSubmissionSuccessURL = (url) =>
-  url.includes('/submissions/detail/') && url.includes('/check/');
+  url.includes("/submissions/detail/") && url.includes("/check/");
 
 const sendUserSolvedMessage = () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'userSolvedProblem' });
+    chrome.tabs.sendMessage(tabs[0].id, { action: "userSolvedProblem" });
   });
 };
 
@@ -17,8 +17,8 @@ const sendUserSolvedMessage = () => {
 
 let leetcodeProblemSolved = false;
 let leetCodeProblem = {
-  url: '',
-  name: '',
+  url: "",
+  name: "",
 };
 let userJustSubmitted = false;
 
@@ -26,7 +26,7 @@ let userJustSubmitted = false;
 const generateRandomLeetCodeProblem = async () => {
   try {
     const res = await fetch(
-      chrome.runtime.getURL('leetCode-problems/blind75Problems.json')
+      chrome.runtime.getURL("leetCode-problems/blind75Problems.json")
     );
     const leetCodeProblems = await res.json();
     const randomIndex = Math.floor(Math.random() * leetCodeProblems.length);
@@ -35,27 +35,27 @@ const generateRandomLeetCodeProblem = async () => {
     const randomProblemName = randomProblem.text;
     return { randomProblemURL, randomProblemName };
   } catch (error) {
-    console.error('Error generating random problem', error);
+    console.error("Error generating random problem", error);
   }
 };
 
 // Communication functions between background.js, popup.js, and content.js
 const onMessageReceived = (message, sender, sendResponse) => {
   switch (message.action) {
-    case 'getProblemStatus':
+    case "getProblemStatus":
       sendResponse({
         problemSolved: leetcodeProblemSolved,
         problem: leetCodeProblem,
       });
       break;
-    case 'userClickedSubmit':
+    case "userClickedSubmit":
       userJustSubmitted = true;
       setTimeout(() => {
         userJustSubmitted = false;
       }, 30000); // Reset after 30 seconds
       break;
     default:
-      console.warn('Unknown message action:', message.action);
+      console.warn("Unknown message action:", message.action);
   }
 };
 
@@ -64,17 +64,17 @@ async function setRedirectRule(newRedirectUrl) {
     id: RULE_ID,
     priority: 1,
     action: {
-      type: 'redirect',
+      type: "redirect",
       redirect: { url: newRedirectUrl },
     },
     condition: {
-      urlFilter: '*://*/*',
+      urlFilter: "*://*/*",
       excludedDomains: [
-        'leetcode.com',
-        'www.leetcode.com',
-        'developer.chrome.com',
+        "leetcode.com",
+        "www.leetcode.com",
+        "developer.chrome.com",
       ],
-      resourceTypes: ['main_frame'],
+      resourceTypes: ["main_frame"],
     },
   };
 
@@ -83,15 +83,15 @@ async function setRedirectRule(newRedirectUrl) {
       removeRuleIds: [RULE_ID],
       addRules: [newRedirectRule],
     });
-    console.log('Redirect rule updated');
+    console.log("Redirect rule updated");
   } catch (error) {
-    console.error('Error updating redirect rule:', error);
+    console.error("Error updating redirect rule:", error);
   }
 }
 const updateStorage = async () => {
   try {
     const currentStorage = await chrome.storage.local.get([
-      'leetCodeProblemSolved',
+      "leetCodeProblemSolved",
     ]);
     if (!currentStorage.leetCodeProblemSolved) {
       try {
@@ -106,20 +106,20 @@ const updateStorage = async () => {
         });
         await setRedirectRule(randomProblemURL);
       } catch (error) {
-        console.error('Error updating storage:', error);
+        console.error("Error updating storage:", error);
       }
     }
   } catch (error) {
-    console.error('Error getting storage:', error);
+    console.error("Error getting storage:", error);
   }
 };
 
 const sendMessageToContentScript = (tabId, message, callback) => {
-  chrome.tabs.sendMessage(tabId, { action: 'ping' }, {}, (response) => {
-    if (response && response.action === 'pong') {
+  chrome.tabs.sendMessage(tabId, { action: "ping" }, {}, (response) => {
+    if (response && response.action === "pong") {
       chrome.tabs.sendMessage(tabId, message, callback);
     } else {
-      console.warn('Content script not ready yet');
+      console.warn("Content script not ready yet");
     }
   });
 };
@@ -129,8 +129,8 @@ const checkIfUserSolvedProblem = async (details) => {
       const response = await fetch(details.url);
       const data = await response.json();
 
-      if (data.status_msg === 'Accepted' && data.state === 'SUCCESS') {
-        console.log('User solved the problem');
+      if (data.status_msg === "Accepted" && data.state === "SUCCESS") {
+        console.log("User solved the problem");
         console.log(
           "Congratulations! You've solved the problem!, I'll see you tomorrow"
         );
@@ -144,7 +144,7 @@ const checkIfUserSolvedProblem = async (details) => {
         sendUserSolvedMessage();
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   }
 };
@@ -154,10 +154,18 @@ chrome.runtime.onInstalled.addListener(async () => {
   await updateStorage();
 });
 // Ensure the alarm is set when the extension starts
-chrome.alarms.get('updateStorage', (alarm) => {
-  //Create an alarm to update the storage every 24 hours
+chrome.alarms.get("updateStorage", (alarm) => {
   if (!alarm) {
-    chrome.alarms.create('updateStorage', { periodInMinutes: 24 * 60 });
+    // Find the time duration until midnight
+    const currentTime = Date.now();
+    const midnight = new Date();
+    midnight.setHours(24, 0, 0, 0);
+    const msUntilMidnight = midnight.getTime() - currentTime;
+    //Create an alarm to update the storage every 24 hours at midnight
+    chrome.alarms.create("updateStorage", {
+      when: Date.now() + msUntilMidnight,
+      periodInMinutes: 24 * 60,
+    });
   }
 });
 //Update the storage when the alarm is fired
@@ -167,5 +175,5 @@ chrome.alarms.onAlarm.addListener(async () => {
 
 chrome.runtime.onMessage.addListener(onMessageReceived);
 chrome.webRequest.onCompleted.addListener(checkIfUserSolvedProblem, {
-  urls: ['*://leetcode.com/submissions/detail/*/check/'],
+  urls: ["*://leetcode.com/submissions/detail/*/check/"],
 });
