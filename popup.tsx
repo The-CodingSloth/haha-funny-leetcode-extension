3fe47daa05cf0f86d6d04a9cc5db8fe97c4204ca
Erik McKelvey <erik.mckelvey.is@gmail.com>
2023-09-10 21:04:36 -0700

    Fixed issues with streaks

--- popup.tsx
+++ popup.tsx
@@ -28,8 +28,8 @@ const IndexPopup = () => {
   const [problemName] = useStorage<string>("problemName")
   const [problemURL] = useStorage<string>("problemURL")
   const [leetcodeProblemSolved] = useStorage<boolean>("leetCodeProblemSolved")
-  const currentStreak = useStorage<number>("currentStreak") ?? 0
-  const bestStreak = useStorage<number>("bestStreak") ?? 0
+  const [currentStreak] = useStorage<number>("currentStreak")
+  const [bestStreak] = useStorage<number>("bestStreak")
 
   return (
     <div>
@@ -51,8 +51,8 @@ const IndexPopup = () => {
       ) : (
         <h2 id="solved-message">{randomSolvedMessage}</h2>
       )}
-      <h2 id="current-streak-message">Current Streak: {+currentStreak}</h2>
-      <h2 id="best-streak-message">Best Streak: {+bestStreak}</h2>
+      <h2 id="current-streak-message">Current Streak: {currentStreak ?? 0}</h2>
+      <h2 id="best-streak-message">Best Streak: {bestStreak ?? 0}</h2>
     </div>
   )
 }
