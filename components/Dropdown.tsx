c40115515e3e8f9e24252715b4e8fc740135b0f2
Tayomide <70061548+Tayomide@users.noreply.github.com>
2023-09-19 13:36:01 -0500

    Add persistent value to dropdown

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react"
+
 const Dropdown = ({ options, defaultValue, handleChange, name }) => {
+  const [value, setValue] = useState(defaultValue || "")
+  const onChange = (e) => {
+    setValue(e.target.value)
+    handleChange(e)
+  }
+  useEffect(() => {
+    setValue(defaultValue)
+  }, [defaultValue])
   return (
     <select
       name={name}
-      onChange={handleChange}
+      onChange={onChange}
       id={name}
       className="setting-dropdown"
-      defaultValue={defaultValue}>
+      value={value}>
       {Object.keys(options).map((value, key) => (
         <option value={value} key={key}>
           {options[value]}
